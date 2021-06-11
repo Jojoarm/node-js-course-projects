f637d1fffbc1d4c0ffdf5806125fa3f1ad846d71
George Ofogba <ofogbageorge3@gmail.com>
2021-06-11 01:25:36 +0100

    second assignment

---
+++ assignment2-express/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+// app.use((req, res, next) => {
+//     console.log('In the first Middleware!');
+//     next()
+// })
+
+// app.use((req, res, next) => {
+//     console.log('In the second Middleware!');
+//     res.send('<h1>The Express test page!</h1>')
+// })
+
+app.use('/users', (req, res, next) => {
+    console.log('In the users Middleware!');
+    res.send('<h1>The users page!</h1>')
+})
+
+app.use('/', (req, res, next) => {
+    console.log('In another Middleware!');
+    res.send('<h1>Hello from my Express Home Page!</h1>')
+})
+
+app.listen(3000)
